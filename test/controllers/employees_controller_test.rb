f8140485c11bf6680d8d038ac2d9cb33df7111d2
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2015-09-02 14:37:14 -0600

    Cover API calls

--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -1,11 +1,13 @@
 require 'test_helper'
 
 class EmployeesControllerTest < ActionController::TestCase
+  should use_before_action :schedule
 
   def setup
     @admin = agents(:daniel)
     @agent = agents(:archer)
 
+    mock_api
   end
 
   #
@@ -16,10 +18,17 @@ class EmployeesControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
 
-  test "should set the employees instance variable when logged in" do
-    stub_request(:get, 'http://interviewtest.replicon.com/employees').
-        to_return(:body => File.new('test/json/employees.json'), :status => 200)
+  test "should set the start_date and end_date instance variables when logged in" do
+    sign_in(@agent)
+    get :index
+    assert_response :success
 
+    # This is hardcoded to start in June, for now
+    assert_equal Date.new(2015, 06, 01), assigns(:start_date)
+    assert_equal Date.new(2015, 06, 28), assigns(:end_date)
+  end
+
+  test "should set the employees instance variable when logged in" do
     sign_in(@agent)
     get :index
     assert_not_nil assigns(:employees)
@@ -34,8 +43,8 @@ class EmployeesControllerTest < ActionController::TestCase
     assert_equal 5, employees[4]['id']
   end
 
-  test "should set an error message if app can't connect to API" do
-    stub_request(:get, 'http://interviewtest.replicon.com/employees').
+  test "should set an error message if app can't get employee list" do
+    stub_request(:get, "#{DOMAIN}/employees").
         to_return(:body => 'Some crazy error message', :status => 500)
 
     sign_in(@agent)
@@ -43,9 +52,68 @@ class EmployeesControllerTest < ActionController::TestCase
     assert_nil assigns(:employees)
     assert_response :success
 
-    assert_equal 'The API cannot be reached: 500', flash[:error]
+    assert_equal 'The employee list could not be retrieved: 500', flash[:error]
   end
 
+  test "should set the shift_rules instance variable when logged in" do
+    sign_in(@agent)
+    get :index
+    assert_not_nil assigns(:shift_rules)
+    assert_response :success
+
+    shift_rules = assigns(:shift_rules)
+    assert_equal 7, shift_rules.count
+
+    assert_equal 1, shift_rules[0]['employee_id']
+    assert_equal 4, shift_rules[0]['rule_id']
+    assert_equal 3, shift_rules[0]['value']
+    assert_nil shift_rules[6]['employee_id']
+    assert_equal 7, shift_rules[6]['rule_id']
+    assert_equal 2, shift_rules[6]['value']
+  end
+
+  test "should set an error message if app can't get shift_rules list" do
+    stub_request(:get, "#{DOMAIN}/shift-rules").
+        to_return(:body => 'Some crazy error message', :status => 500)
+
+    sign_in(@agent)
+    get :index
+    assert_nil assigns(:shift_rules)
+    assert_response :success
+
+    assert_equal 'The shift rules could not be retrieved: 500', flash[:error]
+  end
+
+  test "should set the rule_definitions instance variable when logged in" do
+    sign_in(@agent)
+    get :index
+    assert_not_nil assigns(:rule_definitions)
+    assert_response :success
+
+    rule_definitions = assigns(:rule_definitions)
+    assert_equal 3, rule_definitions.count
+
+    assert_equal 'Minimum number of shifts an employee must work per week. If employee_id is included then this applies to that employee only.',
+        rule_definitions[0]['description']
+    assert_equal 4, rule_definitions[0]['id']
+    assert_equal 'MIN_SHIFTS', rule_definitions[0]['value']
+    assert_equal 'Number of employees required per shift',
+        rule_definitions[2]['description']
+    assert_equal 7, rule_definitions[2]['id']
+    assert_equal 'EMPLOYEES_PER_SHIFT', rule_definitions[2]['value']
+  end
+
+  test "should set an error message if app can't get rule_definitions list" do
+    stub_request(:get, "#{DOMAIN}/ruledefinitions").
+        to_return(:body => 'Some crazy error message', :status => 500)
+
+    sign_in(@agent)
+    get :index
+    assert_nil assigns(:rule_definitions)
+    assert_response :success
+
+    assert_equal 'The rule definitions could not be retrieved: 500', flash[:error]
+  end
 
   #
   # show
