cd966e8885ac72367c8eeda11da356afab383679
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2015-09-02 14:35:16 -0600

    DRY out API mocking

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,29 @@ class ActiveSupport::TestCase
   fixtures :all
 
   include ApplicationHelper
+
+  #
+  # mock_api
+  #
+  # This app makes a lot of API calls, which need to be mocked in various tests
+  DOMAIN = 'http://interviewtest.replicon.com'
+  def mock_api
+    stub_request(:get, "#{DOMAIN}/employees").
+        to_return(:body => File.new('test/json/employees.json'), :status => 200)
+
+    stub_request(:get, "#{DOMAIN}/shift-rules").
+        to_return(:body => File.new('test/json/shift_rules.json'), :status => 200)
+
+    stub_request(:get, "#{DOMAIN}/ruledefinitions").
+        to_return(:body => File.new('test/json/ruledefinitions.json'), :status => 200)
+
+    stub_request(:get, "#{DOMAIN}/weeks/23").
+        to_return(:body => File.new('test/json/week_23.json'), :status => 200)
+
+    stub_request(:get, "#{DOMAIN}/weeks/26").
+        to_return(:body => File.new('test/json/week_26.json'), :status => 200)
+  end
+
 end
 
 # As per https://github.com/plataformatec/devise#test-helpers
