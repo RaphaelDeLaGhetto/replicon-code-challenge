632585307e448ed1fbd412133bfaedc7cab1bd7d
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2015-09-08 16:37:58 -0600

    Get NeedsMoreShifts and ExceedsMaxShifts specifications

--- app/controllers/concerns/schedule_specification.rb
+++ app/controllers/concerns/schedule_specification.rb
@@ -60,6 +60,114 @@ module ScheduleSpecification extend ActiveSupport::Concern
   end
 
   #
+  # Consider the maximum allowable shifts per week 
+  #
+  class ExceedsMaxShifts
+    def initialize(schedule, shift_rules, rule_definitions)
+      @schedule = schedule
+
+      # Find the ID associated with the MAX_SHIFTS rule
+      rule_id = nil
+      rule_definitions.each do |definition|
+        rule_id = definition['id'] if definition['value'] == 'MAX_SHIFTS'
+      end
+
+      # Pick out the applicable rules
+      @shift_rules = []
+      shift_rules.each do |rule|
+        @shift_rules << rule if rule['rule_id'] == rule_id
+      end
+
+      # Determine corporate rule
+      @corporate_max = nil
+      @shift_rules.each do |rule|
+        @corporate_max = rule['value'] if !rule['employee_id'] 
+        break if @corporate_max
+      end
+    end
+
+    def is_satisfied_by?(subject)
+      exceeds_max_shifts = false
+      max_shifts = @corporate_max
+
+      # Determine if this employee has special rules
+      @shift_rules.each do |rule|
+        if rule['employee_id'] && rule['employee_id'] == subject[:employee_id]
+          max_shifts = rule['value']  
+        end
+      end
+
+      # Find the week and count the number of scheduled shifts
+      @schedule.each do |schedule|
+        if schedule[:week] == subject[:week]
+          schedule[:schedules].each do |sched|
+            if sched[:employee_id] == subject[:employee_id]
+              exceeds_max_shifts = sched[:schedule].count >= max_shifts 
+              break
+            end
+          end
+        end
+      end
+
+      exceeds_max_shifts
+    end
+  end
+
+  #
+  # Consider the minimum number of shifts an employee should expect per week 
+  #
+  class NeedsMoreShifts
+    def initialize(schedule, shift_rules, rule_definitions)
+      @schedule = schedule
+
+      # Find the ID associated with the MIN_SHIFTS rule
+      rule_id = nil
+      rule_definitions.each do |definition|
+        rule_id = definition['id'] if definition['value'] == 'MIN_SHIFTS'
+      end
+
+      # Pick out the applicable rules
+      @shift_rules = []
+      shift_rules.each do |rule|
+        @shift_rules << rule if rule['rule_id'] == rule_id
+      end
+
+      # Determine corporate rule
+      @corporate_min = nil
+      @shift_rules.each do |rule|
+        @corporate_min = rule['value'] if !rule['employee_id'] 
+        break if @corporate_min
+      end
+    end
+
+    def is_satisfied_by?(subject)
+      needs_more_shifts = true
+      min_shifts = @corporate_min
+
+      # Determine if this employee has special rules
+      @shift_rules.each do |rule|
+        if rule['employee_id'] && rule['employee_id'] == subject[:employee_id]
+          min_shifts = rule['value']  
+        end
+      end
+
+      # Find the week and count the number of scheduled shifts
+      @schedule.each do |schedule|
+        if schedule[:week] == subject[:week]
+          schedule[:schedules].each do |sched|
+            if sched[:employee_id] == subject[:employee_id]
+              needs_more_shifts = !(sched[:schedule].count >= min_shifts) 
+              break
+            end
+          end
+        end
+      end
+
+      needs_more_shifts
+    end
+  end
+
+  #
   # Determine if an employee is available to work the shift specified
   #
   class IsAvailable
--- test/controllers/concerns/schedule_specifications_test.rb
+++ test/controllers/concerns/schedule_specifications_test.rb
@@ -53,4 +53,111 @@ class DummyControllerTest < ActionController::TestCase
     assert_not timeoff_spec.is_satisfied_by?({ employee_id: 5, week: 26, day: 4 })
     assert_not timeoff_spec.is_satisfied_by?({ employee_id: 2, week: 26, day: 2 })
   end
+
+  #
+  # ExceedsMaxShifts
+  #
+  test "should return true if the employee has too many shifts according to corporate rules" do
+    # Only corporate rules apply to employees 3 and 4 according to test data (max six)
+    schedule = [
+        {
+            "week": 23,
+            "schedules": [
+                { "employee_id": 3, "schedule": [1, 2, 3, 4, 5, 6] },
+                { "employee_id": 4, "schedule": [1, 2, 3, 4, 5, 6, 7] }
+            ]
+        }
+    ]
+    max_shifts_spec = ScheduleSpecification::ExceedsMaxShifts.new(schedule, @shift_rules, @rule_definitions)
+    assert max_shifts_spec.is_satisfied_by?({ employee_id: 3, week: 23 })
+    assert max_shifts_spec.is_satisfied_by?({ employee_id: 4, week: 23 })
+  end
+
+  test "should return true if the employee has too many shifts according to his personal rules" do
+    # The personal rules for employees 1 and 2 are defined in the test data (max five each)
+    schedule = [
+        {
+            "week": 23,
+            "schedules": [
+                { "employee_id": 1, "schedule": [1, 2, 3, 4, 5] },
+                { "employee_id": 2, "schedule": [1, 2, 3, 4, 5, 6] }
+            ]
+        }
+    ]
+    max_shifts_spec = ScheduleSpecification::ExceedsMaxShifts.new(schedule, @shift_rules, @rule_definitions)
+    assert max_shifts_spec.is_satisfied_by?({ employee_id: 1, week: 23 })
+    assert max_shifts_spec.is_satisfied_by?({ employee_id: 2, week: 23 })
+  end
+
+  test "should return false if the employee has not reached the shift limit" do
+    schedule = [
+        {
+            "week": 23,
+            "schedules": [
+                { "employee_id": 1, "schedule": [1, 2, 3, 4] },
+                { "employee_id": 3, "schedule": [1, 2, 3, 4, 5] }
+            ]
+        }
+    ]
+
+    max_shifts_spec = ScheduleSpecification::ExceedsMaxShifts.new(schedule, @shift_rules, @rule_definitions)
+    assert_not max_shifts_spec.is_satisfied_by?({ employee_id: 1, week: 23 })
+    assert_not max_shifts_spec.is_satisfied_by?({ employee_id: 2, week: 23 })
+    assert_not max_shifts_spec.is_satisfied_by?({ employee_id: 3, week: 23 })
+    assert_not max_shifts_spec.is_satisfied_by?({ employee_id: 4, week: 23 })
+  end
+
+  #
+  # NeedsMoreShifts
+  #
+  test "should return true if the employee doesn't have enough shifts according to corporate rules" do
+    # Only corporate rules apply to employees 3 and 4 according to test data (min 2)
+    schedule = [
+        {
+            "week": 23,
+            "schedules": [
+                { "employee_id": 3, "schedule": [1] }
+            ]
+        }
+    ]
+    min_shifts_spec = ScheduleSpecification::NeedsMoreShifts.new(schedule, @shift_rules, @rule_definitions)
+    assert min_shifts_spec.is_satisfied_by?({ employee_id: 3, week: 23 })
+    assert min_shifts_spec.is_satisfied_by?({ employee_id: 4, week: 23 })
+  end
+
+  test "should return true if the employee doesn't have enough shifts according to his personal rules" do
+    # The personal rules for employees 1 and 2 are defined in the test data (min 3 and 5, respectively)
+    schedule = [
+        {
+            "week": 23,
+            "schedules": [
+                { "employee_id": 1, "schedule": [1, 2] },
+                { "employee_id": 2, "schedule": [1, 2, 3, 4] }
+            ]
+        }
+    ]
+    min_shifts_spec = ScheduleSpecification::NeedsMoreShifts.new(schedule, @shift_rules, @rule_definitions)
+    assert min_shifts_spec.is_satisfied_by?({ employee_id: 1, week: 23 })
+    assert min_shifts_spec.is_satisfied_by?({ employee_id: 2, week: 23 })
+  end
+
+  test "should return false if the employee meets or exceeds the minimum required shifts" do
+    schedule = [
+        {
+            "week": 23,
+            "schedules": [
+                { "employee_id": 1, "schedule": [1, 2, 3, 4, 5] },
+                { "employee_id": 2, "schedule": [1, 2, 3, 4, 5] },
+                { "employee_id": 3, "schedule": [1, 2] },
+                { "employee_id": 4, "schedule": [1, 2] }
+            ]
+        }
+    ]
+
+    min_shifts_spec = ScheduleSpecification::NeedsMoreShifts.new(schedule, @shift_rules, @rule_definitions)
+    assert_not min_shifts_spec.is_satisfied_by?({ employee_id: 1, week: 23 })
+    assert_not min_shifts_spec.is_satisfied_by?({ employee_id: 2, week: 23 })
+    assert_not min_shifts_spec.is_satisfied_by?({ employee_id: 3, week: 23 })
+    assert_not min_shifts_spec.is_satisfied_by?({ employee_id: 4, week: 23 })
+  end
 end
